7e701237bb11b233be41f055b202fd51fcc472bc
Giuseppe Barillari <joe2k01dev@gmail.com>
2020-08-07 01:43:24 +0200

    Drop ArduinoJson library on the uno and switch to an epd
     * Send a char array from the esp instead of a json object
     * Switch to a WaveShare 4.2" e-paper display
     * Also include OpenWeather icons
    
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,50 +1,104 @@
-#include <ArduinoJson.h>
-#include <LiquidCrystal_I2C.h>
+#define ENABLE_GxEPD2_GFX 0
+
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
 #include <SoftwareSerial.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
-SoftwareSerial mSerial(4, 7);
-StaticJsonDocument<200> doc;
+#include "icons.h"
 
-int temp, humidity;
-String city;
+#define W 100 // Icon Width
+#define H 100 // Icon Height
+
+#define MAX_DISPLAY_BUFFER_SIZE 800
+#define MAX_HEIGHT(EPD) (EPD::HEIGHT <= MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8) ? EPD::HEIGHT : MAX_DISPLAY_BUFFER_SIZE / (EPD::WIDTH / 8))
+
+GxEPD2_BW<GxEPD2_420, MAX_HEIGHT(GxEPD2_420)> display(GxEPD2_420(/*CS*/ 10, /*DC=*/ 9, /*RST=*/ 8, /*BUSY=*/ 7));
+SoftwareSerial mSerial(4, 2);
+
+int textHeight;
+
+void displaySetUp(bool initial) {
+  display.init();
+  display.setRotation(0);
+  display.setFont(&FreeMonoBold12pt7b);
+  display.setTextColor(GxEPD_BLACK);
+
+  if (initial) {
+    display.setFullWindow();
+    char retr[] = "Retrieving Data";
+    int a, b, c;
+    display.getTextBounds(retr, 0, 0, &a, &b, &c, &textHeight);
+  
+    display.firstPage();
+    do {
+      display.fillScreen(GxEPD_WHITE);
+      display.setCursor(0, textHeight);
+      display.print(retr);
+    } while (display.nextPage());
+  }
+}
 
 void setup() {
   Serial.begin(9600);
   mSerial.begin(2400);
-  lcd.init();
-  lcd.backlight();
+  displaySetUp(true);
 }
 
 void loop() {
-  String json;
-   while (mSerial.available()) {
-   if (mSerial.available() > 0 ) {
-    json = mSerial.readString();
-    Serial.println(json);
-   }
-   DeserializationError error = deserializeJson(doc, json);
-   if (error) {
-    Serial.println(error.c_str());
-   } else {
-    Serial.println("Succes");
-   }
-
-   if (temp != doc["temp"].as<int>() || humidity != doc["humidity"] || city != doc["city"]) {
-    temp = doc["temp"].as<int>();
-    humidity = doc["humidity"].as<int>();
-    city = doc["city"].as<String>();
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Temperature: ");
-    lcd.print(temp);
-    lcd.setCursor(0, 1);
-    lcd.print("Humidity: ");
-    lcd.print(humidity);
-    lcd.setCursor(0, 2);
-    lcd.print("City: ");
-    lcd.setCursor(0, 3);
-    lcd.print(city);
-   }
+  char message[200];
+  
+  while (!mSerial.available()) {
+    // Do nothing
   }
+
+  if (mSerial.available() > 0) {
+    /*
+     * message is a char array of this kind: 1199#City@
+     * 11: are temperature digits
+     * 99: are humidity digits
+     * #: marks the start of the location name
+     * City: is the location name 
+     * @: marks the end of the location name
+     */
+    int index = 0;
+    char c = mSerial.read();
+    while (c != '@') { // Read until @ and exclude it
+      if (isAscii(c)) { // Make sure valid characters are coming in
+        message[index] = c;
+        index++;
+      }
+      c = mSerial.read();
+    }
+    message[index] = '\0'; // Terminate the string
+  }
+  Serial.println(message);
+  int temp = (10 * (message[0] - '0')) + (message[1] - '0'); // char[n] - '0' converts char[n] to an integer; multiply by 10 and add char[n+1] - '0' to concatenate 2 digits into one int
+  int humidity;
+  if(message[4] == '#') {
+    humidity = (10 * (message[2] - '0')) + (message[3] - '0');
+  } else {
+    humidity = 100;
+  }
+  int index = 0;
+  for (int i = 0; i < strlen(message); i++) {
+    if (message[i] == '#') // Define the offset at which the location name starts
+      index = i + 1;
+  }
+  char location[50];
+  int j = 0;
+  for (index; index < strlen(message); index++) {
+    location[j] = message[index];
+    j++;
+  }
+  location[j] = '\0';
+  display.firstPage();
+    do {
+      display.fillScreen(GxEPD_WHITE);
+      display.setCursor(0, textHeight);
+      display.print(temp);
+      display.setCursor(0, 2 * textHeight);
+      display.print(humidity);
+      display.setCursor(0, 3 * textHeight);
+      display.print(location);
+    } while (display.nextPage());
 }
--- esp/esp.ino
+++ esp/esp.ino
@@ -7,7 +7,6 @@
 #include "config.h"
 
 SoftwareSerial mSerial(0, 2);
-StaticJsonDocument<JSON_OBJECT_SIZE(3)> doc;
 ESP8266WebServer server(80);
 HTTPClient http;
 char url[200];
@@ -17,21 +16,29 @@ void fetchWeather(String location) {
   location.toCharArray(locationChar, 100);
   sprintf(url, "http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s", locationChar, APPID);
   http.begin(url);
-    if (http.GET() == 200) {
-      DynamicJsonDocument weather(1200);
-      deserializeJson(weather, http.getString());
-      
-      int temp = weather["main"]["temp"].as<int>() / 10;
-      int humidity = weather["main"]["humidity"].as<int>();
-      auto location = weather["name"].as<const char*>();
+  if (http.GET() == 200) {
+    DynamicJsonDocument weather(1200);
+    deserializeJson(weather, http.getString());
 
-      doc["temp"] = temp;
-      doc["humidity"] = humidity;
-      doc["city"] = location;
-    }
+    int temp = weather["main"]["temp"].as<int>() / 10;
+    int humidity = weather["main"]["humidity"].as<int>();
+    auto locationName = weather["name"].as<const char*>();
+
+    int messageSize = location.length() + 6;
+    char message[messageSize];
+    /*
+     * message is a char array of this kind: 1199#City@
+     * 11: are temperature digits
+     * 99: are humidity digits
+     * #: marks the start of the location name
+     * City: is the location name 
+     * @: marks the end of the location name
+     */
+    sprintf(message, "%d%d#%s@", temp, humidity, locationName);
+    mSerial.write(message);
+    Serial.write(message);
+  }
   http.end();
-  serializeJson(doc, mSerial);
-  serializeJson(doc, Serial);
 }
 
 void handleRoot() {
@@ -43,7 +50,7 @@ void handleRoot() {
     int addresses = location.length() + 1;
     EEPROM.write(0, addresses);
     for (int i = 1; i < addresses; i++) {
-      EEPROM.write(i, locationBuffer[i-1]);
+      EEPROM.write(i, locationBuffer[i - 1]);
     }
     EEPROM.commit();
     fetchWeather(location);
@@ -56,21 +63,21 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
-  
+
   mSerial.begin(2400);
   Serial.begin(9600);
   Serial.println(WiFi.localIP());
 
   delay(2000);
-  
+
   EEPROM.begin(512);
   int addresses = EEPROM.read(0);
   if (addresses > 0) {
     char mLoc[addresses];
     for (int i = 1; i < addresses; i++) {
-      mLoc[i-1] = EEPROM.read(i);
+      mLoc[i - 1] = EEPROM.read(i);
     }
-    mLoc[addresses-1] = '\0';
+    mLoc[addresses - 1] = '\0';
     fetchWeather(String(mLoc));
   }
 
